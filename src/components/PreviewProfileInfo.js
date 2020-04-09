83947170ae0b9e1156af6e5d107860cf784f3cb0
mablancoalvarez <mablancoalvarez@gmail.com>
2020-04-09 18:46:34 +0200

    add color in cardPreview

--- src/components/PreviewProfileInfo.js
+++ src/components/PreviewProfileInfo.js
@@ -5,15 +5,15 @@ import '../stylesheets/Preview.scss';
 class PreviewProfileInfo extends React.Component {
   constructor(props) {
     super(props);
+   
   }
   render() {
     return (
-
-      // infoStyle1  {`ralph-wrapper ${props.colorPalette === '1' ? "red" : props.colorPalette === '2' ? "blue" : "green"}`}>
-      
-      // "card__profileInfo--name"
-      <div id="PreviewProfileInfo" className="card__profileInfo--box">
-          <p id="PreviewProfileInfoName" className={`card__profileInfo--name ${this.props.value === "1" ? "red" : this.props.checked === "2" ? "red" : this.props.checked === "3" ? "red" : "red"}`}>
+      // card__profileInfo--box
+      // card__profileInfo--name
+    
+      <div id="PreviewProfileInfo" className={`card__profileInfo--box ${this.props.value === "1" ? "red" : this.props.value === "2" ? "black" : this.props.value === "3" ? "red" : "infoStyle1"}`}>
+          <p id="PreviewProfileInfoName" className="card__profileInfo--name" >
           {(this.props.name === '' ? 'Nombre Apellido' : this.props.name)}
           </p>
           <p id="PreviewProfileInfoJob" className="card__profileInfo--job">{(this.props.job === '' ? 'Front-End Developer' : this.props.job)}
